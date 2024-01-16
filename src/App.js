1264626177a8d91ac4b20328f0afc2a3d721c94c
saniya-farheen <saniyafarheen188@gmail.com>
2024-01-16 23:32:34 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Profile from './components/Profile';
+
+
+
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Home/> }></Route>
+        <Route path='/signup' element={ <SignUp/> }></Route>
+        <Route path='/signin' element={ <SignIn/> }></Route>
+        <Route path='/profile' element={ <Profile/> }></Route>
+      </Routes>
+     
     </div>
+    </BrowserRouter>
   );
 }
 
